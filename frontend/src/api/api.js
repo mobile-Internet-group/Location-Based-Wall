3e21eaf1b76fbdb893c630d947f98a5a01e8deea
Yinlong Zhang <71436733+silvercape@users.noreply.github.com>
2023-06-01 20:04:38 +0800

    Delete frontend directory
    remove vue frontend

--- frontend/src/api/api.js
+++
@@ -1,8 +0,0 @@
-// frontend/src/api/api.js
-import axiosInstance from './index'
-
-const axios = axiosInstance
-
-export const getWalls = () => {return axios.get(`http://localhost:8000/api/walls/`)}
-
-export const postWall = (wallName, wallAuthor) => {return axios.post(`http://localhost:8000/api/walls/`, {'name': wallName, 'author': wallAuthor})}
